0ff5c13c0403228a0bcd6c42ceaeb410b3787697
Bruno Marques <blmarques.dev@gmail.com>
2021-10-18 22:07:39 -0300

    Version 001 done

--- src/app.js
+++ src/app.js
@@ -6,19 +6,22 @@ const dataTry = (data) => {
   }
 }
 
-const cep = document.querySelector('#cep');
-cep.addEventListener('blur', (e) => {
-  const cepValue = cep.value.replace('-', '');
-  const options = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'default'
-  }
-  
-  fetch(`https://viacep.com.br/ws/${cepValue}/json/`, options)
+const getData = (cep) => {
+  const options = { method: 'GET', mode: 'cors', cache: 'default' }
+
+  fetch(`https://viacep.com.br/ws/${cep}/json/`, options)
     .then((response) => {
       response.json()
-      .then((data) => dataTry(data))
+        .then((data) => dataTry(data))
     })
     .catch((err) => console.log(`Deu ruim ${err.message}`))
+}
+
+const cep = document.querySelector('#cep');
+cep.addEventListener('blur', (e) => {
+  const cepValue = cep.value.replace(/\D/g, '');
+  if (cepValue.lenght !== 8) {
+    return getData(cepValue);
+  }
+  return alert('O CEP informado é invalido, utilize o o faormato 00000-000')
 });
